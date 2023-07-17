277e672e53a4b811e3a015238cb8b305db658d9d
ogamache <o.gamache2@gmail.com>
2023-07-17 16:14:52 -0400

    Added docker files

---
+++ docker/config.sh
@@ -0,0 +1,5 @@
+REPOS_NAME="mono_dataset_code"
+LOCAL_HOME=$HOME
+DOCKER_HOME="/home/user"
+REPOS_FOLDER="${LOCAL_HOME}/Documents/ICRA2024/repos/${REPOS_NAME}"
+DATA_FOLDER="${LOCAL_HOME}/Documents/ICRA2024/dataset/images/"
